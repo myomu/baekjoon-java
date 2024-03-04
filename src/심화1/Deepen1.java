23c7185445e6ed6327564a550911b8a628f49f7b
myoumu <ayan9414@gmail.com>
2024-03-04 23:00:35 +0900

    2024.03.04

---
+++ src/심화1/Deepen1.java
@@ -0,0 +1,18 @@
+package 심화1;
+
+/**
+ * 25083번
+ * 새싹
+ */
+public class Deepen1 {
+    public static void main(String[] args) {
+        System.out.print("         ,r'\"7" + "\n"
+                         + "r`-_   ,'  ,/" + "\n"
+                         + " \\. \". L_r'" + "\n"
+                         + "   `~\\/" + "\n"
+                         + "      |" + "\n"
+                         + "      |"
+        );
+    }
+
+}
