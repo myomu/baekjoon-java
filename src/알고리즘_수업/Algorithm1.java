c282f65dbd29f7cc46eea2295799679ddda17a19
myomu <ayan9414@gmail.com>
2024-05-20 11:08:00 +0900

    2024.05.20

---
+++ src/알고리즘_수업/Algorithm1.java
@@ -0,0 +1,12 @@
+package 알고리즘_수업;
+
+/**
+ * 24262번
+ * 알고리즘 수업 - 알고리즘의 수행 시간 1
+ */
+public class Algorithm1 {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.print(0);
+    }
+}
---
+++ src/알고리즘_수업/Algorithm3.java
@@ -0,0 +1,20 @@
+package 알고리즘_수업;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 24264번
+ * 알고리즘 수업 - 알고리즘의 수행 시간 3
+ */
+public class Algorithm3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Long n = Long.parseLong(br.readLine());
+        System.out.println(n*n);
+        System.out.println(2);
+
+    }
+}
