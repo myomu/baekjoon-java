7c637e31415eacfc24be9e9162ab6a69cd37fffd
myomu <ayan9414@gmail.com>
2024-06-11 18:05:51 +0900

    2024.06.11

---
+++ src/sort/Sort1.java
@@ -0,0 +1,26 @@
+package sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/**
+ * 2750번
+ * 수 정렬하기
+ */
+public class Sort1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
